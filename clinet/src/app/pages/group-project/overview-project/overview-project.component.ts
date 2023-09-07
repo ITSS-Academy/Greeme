922827f347b597be9b95c970ee252922c43b8d1d
minhhu <minh.nq40153@sinhvien.hoasen.edu.vn>
2023-09-07 22:25:02 +0700

    last days

--- clinet/src/app/pages/group-project/overview-project/overview-project.component.ts
+++ clinet/src/app/pages/group-project/overview-project/overview-project.component.ts
@@ -29,12 +29,37 @@ export class OverviewProjectComponent implements OnInit {
     protected projectService: ProjectService,
     protected issueService: IssueService) {
     this.events = [
-      { teamMember: 'Minh',status: 'Comments', date: '29/8/2023 10:30', icon: 'pi pi-chevron-circle-right', color: "#0A7D56"},
-      { teamMember: 'Wind',status: 'Edits', date: '15/8/2023 14:00', icon: 'pi pi-chevron-circle-left', color: "#0A7D56"},
-      { teamMember: 'Liam',status: 'Edits', date: '15/8/2023 16:15', icon: 'pi pi-chevron-circle-right', color: "#0A7D56"},
-      { teamMember: 'Bong',status: 'Edits', date: '14/8/2023 14:00', icon: 'pi pi-chevron-circle-left', color: "#0A7D56"},
-      { teamMember: 'Mr.Huan',status: 'Edits', date: '13/8/2023 16:15', icon: 'pi pi-chevron-circle-right', color: "#0A7D56"},
-      { teamMember: 'Mr.Huan',status: 'Creates Project', date: '29/7/2023 10:00', icon: 'pi pi-chevron-circle-left', color: "#0A7D56"}
+      { teamMember: 'Minh', status: 'Comment', date: '15/10/2020 10:30', icon: 'pi pi-chevron-right', color: "#0A7D56" },
+      { teamMember: 'Wind', status: 'Edit', date: '15/10/2020 14:00', icon: 'pi pi-chevron-left', color: "#0A7D56" },
+      { teamMember: 'Liam', status: 'Edit', date: '15/10/2020 16:15', icon: 'pi pi-circle-fill', color: "#0A7D56" },
+      { teamMember: 'Bong', status: 'Edit', date: '15/10/2020 14:00', icon: 'pi pi-circle-fill', color: "#0A7D56" },
+      { teamMember: 'Mr.Huan', status: 'Edit', date: '15/10/2020 16:15', icon: 'pi pi-circle-fill', color: "#0A7D56" },
+      { teamMember: 'Mr.Huan', status: 'Create Project', date: '16/10/2020 10:00', icon: 'pi pi-circle-fill', color: "#0A7D56" }
+    ];
+    this.members = this.memProjectService.menbersProject;
+    this.projectService.getProjectById(this.projectService.idCurrentProject).subscribe((data: Project) => {
+      this.project = data;
+      this.project.created_on= new Date(this.project.created_on).toLocaleDateString('en-GB');
+      this.issueService.getProgressTastsInProject().then((data: number) => {
+        this.progress = data;
+      });
+    });
+    this.responsiveOptions = [
+      {
+          breakpoint: '1200px',
+          numVisible: 5,
+          numScroll: 5
+      },
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 1,
+          numScroll: 1
+      }
   ];
   }
 
@@ -45,22 +70,22 @@ export class OverviewProjectComponent implements OnInit {
 
   }
 
-  getSeverity(status: string):  string  {
-    switch (status.toLowerCase()) {
-        case 'completed':
+  getSeverity(status: number):  string  {
+    switch (status) {
+        case 1:
             return 'success';
             break;
-        case 'ongoing':
+        case 2:
             return 'info';
             break;
-        case 'delayed':
-            return 'warning';
-            break;
-        case 'cancelled':
+        // case 'delayed':
+        //     return 'warning';
+        //     break;
+        case 0:
             return 'danger';
             break;
         default:
-            return 'info';
+            return 'success';
             break;
     }
   }
